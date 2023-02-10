85d54563db22e9a7818e9ecf94c71e905f925775
Leon <leon@leon.com>
2023-02-10 20:14:27 +0000

    introduce separate files

--- src/Payment.tsx
+++ src/Payment.tsx
@@ -1,40 +1,5 @@
-import { useEffect, useState } from "react"
-import { convertPaymentMethods } from "./convertPaymentMethods"
-import { LocalPaymentMethod, RemotePaymentMethod } from "./LocalPaymentMethod"
-
-const usePaymentMethods = () => {
-  const [paymentMethods, setPaymentMethods] = useState<LocalPaymentMethod[]>([])
-
-  useEffect(() => {
-    const fetchPaymentMethods = async () => {
-      const url = 'http://localhost:3000/api/payment-methods'
-      const response = await fetch(url)
-      console.log(response)
-      const methods: RemotePaymentMethod[] = await response.json()
-      const extended: LocalPaymentMethod[] = convertPaymentMethods(methods)
-      setPaymentMethods(extended)
-    }
-    fetchPaymentMethods()
-      .catch(console.error)
-  },[])
-    
-  return paymentMethods
-}
-
-const PaymentMethods = ({paymentMethods}: {paymentMethods: LocalPaymentMethod[]}) => (
-    <>
-      {paymentMethods.map((method) => (
-          <label key={method.provider}>
-            <input
-              type="radio"
-              name="payment"
-              value={method.provider}
-              defaultChecked={method.isDefault}
-            />
-            <span>{method.label}</span>
-          </label>
-        ))}
-    </>)
+import { PaymentMethods } from "./PaymentMethods"
+import { usePaymentMethods } from "./usePaymentMethods"
 
 export const Payment = () => {
   const paymentMethods = usePaymentMethods()
